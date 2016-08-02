d198ca63b6df09ded1af5fdabc0b75f2a7fa6831
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-02 01:08:54 -0500

    other ways to create arrays

--- app.js
+++ app.js
@@ -4,3 +4,22 @@
 var people = new Array("Bucky", "Tommy", "Sarah", "Hanah", "Mikety");
 
 document.write(people[0]);
+document.write("<br>");
+
+/****************************************************
+*            Other Ways to Create Arrays            *
+****************************************************/
+var things = new Array(3);
+
+things[0] = "jersey shore";
+things[1] = "brian regan";
+things[2] = "thenewboston";
+
+document.write(things[1]);
+
+var stuff = new Array();
+
+stuff[0] = "toolbox";
+stuff[1] = 123;
+
+document.write(stuff[1]);
